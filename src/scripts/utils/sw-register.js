bcdafd4b3d3c9997b891de2eb018df732d131726
anisamsrh <nuranisahidayatulmasruroh@gmail.com>
2023-06-27 21:34:02 +0700

    using Workbox for generating Service Worker

--- src/scripts/utils/sw-register.js
+++ src/scripts/utils/sw-register.js
@@ -1,17 +1,3 @@
-// const swRegister = async () => {
-//   if (!('serviceWorker' in navigator)) {
-//     console.log('Browser tidak mendukung Service Worker');
-//   }
-
-//   try {
-//     await navigator.serviceWorker.register('./sw.bundle.js');
-//     console.log('SW is registered');
-//   } catch (error) {
-//     console.log('Failed to register SW', error);
-//   }
-// };
-
-// export default swRegister;
 import * as WorkboxWindow from 'workbox-window';
 
 const swRegister = async () => {
